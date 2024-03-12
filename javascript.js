e09518d4ab5d776184da0ef653baf37fa4bfbd7d
Raventh23 <blazenether@gmail.com>
2024-03-12 15:17:35 -0400

    Update computer choice function to return words updated the computerchoice function to return a word like Scissor or Rock rather than 1 or 0 that represent the options

--- javascript.js
+++ javascript.js
@@ -1,3 +1,13 @@
 function getComputerChoice(){
-    let getComputerChoice = Math.floor(Math.random() * 3)
+    let randomChoice = Math.floor(Math.random() * 3);
+    if (randomChoice == 0){
+        return "Rock";
+    } else if (randomChoice == 1){
+        return "Paper";
+    } else if (randomChoice == 2){
+        return "Scissors";
+    }
+
+    return getComputerChoice ;
 }
+console.log(getComputerChoice());
